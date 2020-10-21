62d75381f82bac98460eb8d01054dc65e59c9f4a
Johan van Rhyn <github@jvanrhyn.co.za>
2020-10-21 22:20:24 +0200

    Added WithIndex extension method.

---
+++ Talista.Extensions.Tests/ExtensionTests.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Talista.Extensions.Tests
+{
+    public class ExtensionTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Can_Get_IndexedEnumerable_From_List()
+        {
+	        var source = new List<string> {"one", "two", "three"};
+
+	        var result = source.WithIndex();
+
+	        var expected = new List<(int, string)> {(0, "one"), (1, "two"), (2, "three")};
+
+	        result.ShouldContain(x => expected.Contains(x));
+        }
+
+        [Test]
+        public void Can_Get_IndexedEnumerable_From_List_OneBased()
+        {
+	        var source = new List<string> {"one", "two", "three"};
+
+	        var result = source.WithIndex(zeroBased: false);
+
+	        var expected = new List<(int, string)> {(1, "one"), (2, "two"), (3, "three")};
+
+	        result.ShouldContain(x => expected.Contains(x));
+        }
+    }
+}
---
+++ Talista.Extensions/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Based on the post https://khalidabuhakmeh.com/implement-kotlins-withindex-in-csharp
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Talista.Extensions
+{
+    public static class EnumerableExtensions
+    {
+	    /// <summary>
+	    /// Get an indexed enumerable tuple(int, T) for the source list
+	    /// </summary>
+	    /// <param name="source">Source IEnumerable[T]</param>
+	    /// <param name="zeroBased">When true (default) indexing starts af zero. When false, indexing starts at one.</param>
+	    /// <typeparam name="T"></typeparam>
+	    /// <returns></returns>
+	    public static IEnumerable<(int, T)> WithIndex<T>(this IEnumerable<T> source, bool zeroBased = true )
+	    {
+		    var positionShift = zeroBased ? 0 : 1;
+		    return source.Select((value, index) => ((index + positionShift), value));
+	    }
+    }
+}
